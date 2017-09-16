f378e07f3abf372207280350738cf07da459b6b6
sdczzm <fb112358132134@163.com>
2017-09-16 12:16:45 +0800

    wcmDemo

---
+++ src/main/java/com/sdczzm/wcmDemo/WcmDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sdczzm.wcmDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcmDemoApplication.class, args);
+	}
+}
